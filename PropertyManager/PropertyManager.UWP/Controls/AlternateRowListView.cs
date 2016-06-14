777fb0f249d96709b0dfee8d4e7966f5823a0af8
simonjaeger <simon.jager@live.com>
2016-06-15 00:31:33 +0200

    Updated GroupView

---
+++ PropertyManager/PropertyManager.UWP/Controls/AlternateRowListView.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace PropertyManager.UWP.Controls
+{
+    public class AlternateRowListView : ListView
+    {
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+            var index = ItemContainerGenerator.IndexFromContainer(element);
+            var listViewItem = element as ListViewItem;
+            if (listViewItem == null)
+            {
+                return;
+            }
+
+            if (index % 2 == 0)
+            {
+                listViewItem.Background =
+                    Application.Current.Resources["AlternateRowListViewItemBackgroundColorBrush"] 
+                    as SolidColorBrush;
+            }
+        }
+    }
+}
