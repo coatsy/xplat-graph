1edd4a3708512de0d7382a990f9c1d136440a89a
Simon Jaeger <simon.jager@live.com>
2016-07-24 12:27:56 +0200

    Updated DetailsTalslsTabView

--- src/PropertyManager/PropertyManager.iOS/Views/Tabs/DetailsTabView.cs
+++ src/PropertyManager/PropertyManager.iOS/Views/Tabs/DetailsTabView.cs
@@ -1,4 +1,6 @@
-﻿using MvvmCross.iOS.Views;
+﻿using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
 using PropertyManager.ViewModels;
 using UIKit;
 
@@ -29,12 +31,28 @@ namespace PropertyManager.iOS
 			var rightNavigationButton = new UIBarButtonItem(UIBarButtonSystemItem.Edit, (sender, e) =>
 			                                                viewModel.EditDetailsCommand.Execute(null));
 			NavigationItem.RightBarButtonItem = rightNavigationButton;
+
+			// Create and apply the binding set.
+			var set = this.CreateBindingSet<DetailsTabView, GroupViewModel>();
+			set.Bind(DescriptionLabel).To(vm => vm.Details.Description);
+			set.Bind(RoomsLabel).To(vm => vm.Details.Rooms);
+			set.Bind(LivingAreaLabel).To(vm => vm.Details.LivingArea);
+			set.Bind(LotSizeLabel).To(vm => vm.Details.LotSize);
+			set.Bind(OperatingCostsLabel).To(vm => vm.Details.OperatingCosts);
+			set.Apply();
 		}
 
 		public override void DidReceiveMemoryWarning()
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+			var frame = ContentView.Frame;
+			ScrollView.ContentSize = new CoreGraphics.CGSize(frame.Size.Width, frame.Size.Height + 49);
 		}
 	}
 }
--- src/PropertyManager/PropertyManager.iOS/Views/Tabs/DetailsTabView.designer.cs
+++ src/PropertyManager/PropertyManager.iOS/Views/Tabs/DetailsTabView.designer.cs
@@ -1,22 +1,74 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace PropertyManager.iOS
 {
-	[Register("DetailsTabView")]
+	[Register ("DetailsTabView")]
 	partial class DetailsTabView
 	{
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		UIKit.UIView ContentView { get; set; }
+
+		[Outlet]
+		UIKit.UILabel DescriptionLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel LivingAreaLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel LotSizeLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel OperatingCostsLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel RoomsLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIScrollView ScrollView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
+			if (DescriptionLabel != null) {
+				DescriptionLabel.Dispose ();
+				DescriptionLabel = null;
+			}
+
+			if (RoomsLabel != null) {
+				RoomsLabel.Dispose ();
+				RoomsLabel = null;
+			}
+
+			if (LivingAreaLabel != null) {
+				LivingAreaLabel.Dispose ();
+				LivingAreaLabel = null;
+			}
+
+			if (LotSizeLabel != null) {
+				LotSizeLabel.Dispose ();
+				LotSizeLabel = null;
+			}
+
+			if (OperatingCostsLabel != null) {
+				OperatingCostsLabel.Dispose ();
+				OperatingCostsLabel = null;
+			}
+
+			if (ScrollView != null) {
+				ScrollView.Dispose ();
+				ScrollView = null;
+			}
+
+			if (ContentView != null) {
+				ContentView.Dispose ();
+				ContentView = null;
+			}
 		}
 	}
 }
-
